ce0abc616dc78cd42ad92386fe204206541c1fbf
lilly culp <112136351+lmc00024@users.noreply.github.com>
2022-11-08 19:46:48 -0500

    deployed app !! and connected firebase stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,27 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXFSodr9mXJnCQIOPcZ9noj_NIzuBIKBc",
+  authDomain: "pandora-app-6ecf2.firebaseapp.com",
+  databaseURL: "https://pandora-app-6ecf2-default-rtdb.firebaseio.com",
+  projectId: "pandora-app-6ecf2",
+  storageBucket: "pandora-app-6ecf2.appspot.com",
+  messagingSenderId: "267823474289",
+  appId: "1:267823474289:web:a604cb93d0ee11ed325dd0",
+  measurementId: "G-LT0K0WZWJ2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
