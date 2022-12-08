45ddcf6abe9e25c4b6b0b418f62471018e26e614
lilly culp <112136351+lmc00024@users.noreply.github.com>
2022-12-08 15:05:17 -0500

    added login method and created auth service

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { AuthResponse } from './authResponse';
 
 @Component({
   selector: 'app-auth',
@@ -8,13 +11,38 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  public buttonClicked!:string;
+  private authObservable!: Observable<AuthResponse>;
+
+  constructor(private authService:AuthService) {
+      
+  }
 
   ngOnInit(): void {
   }
-  public onSubmit(data: NgForm) {
-    console.log("button clicked");
-    console.log(data.value);
-  }
 
+  public onSubmit(data: NgForm) {
+    
+    console.log("button clicked = " + this.buttonClicked);
+    console.log(data.value);
+
+    if (this.buttonClicked == 'SignUp') {
+      this.authObservable = this.authService.signup(data.value.email, data.value.password);
+    }
+    if (this.buttonClicked == 'Login') {
+      this.authObservable = this.authService.login(data.value.email, data.value.password);
+    }
+    
+      
+    this.authObservable.subscribe(
+      (data: AuthResponse) => {
+        console.log(data);
+      },
+      (error: any) => {
+        console.log(error.error);
+      }
+    );
+    data.resetForm();
+  }
+  
 }
---
+++ src/app/auth/auth/authResponse.ts
@@ -0,0 +1,11 @@
+
+
+
+export interface AuthResponse {
+    idToken:string,
+    email:string,
+    refreshToken:string,
+    expiresIn:string, 
+    localId:string,
+    registered?:boolean
+}
